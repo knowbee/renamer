6c6537878468a73106e1c4bae1e7467b6ccb6e9c
knowbee <knowbeeinc@gmail.com>
2020-02-25 17:56:41 +0200

    remove trash

--- src/renamer.js
+++ src/renamer.js
@@ -1,40 +1,38 @@
-const color  = require('chalk')
-const { rename } = require("fs");
+const color = require('chalk')
+const {
+  rename
+} = require("fs");
 
 module.exports = {
   /**
- * Renames file names
- *
- * @param {String} file
- * @param {String} answers
- * @param {String} dirname
- * @returns {<String>} A new file name.
- */
-   renamer : (file, answers, dirname) =>{
-      if (file.match(`${answers.match}`)) {
-        let newname
-        if(answers.match.includes('.') && file.split(`${answers.match}`)[1] === ''){
+   * Renames file names
+   *
+   * @param {String} file
+   * @param {String} answers
+   * @param {String} dirname
+   * @returns {<String>} A new file name.
+   */
+  renamer: (file, answers, dirname) => {
+    if (file.match(`${answers.match}`)) {
+      let newname
+      if (answers.match.includes('.') && file.split(`${answers.match}`)[1] === '') {
+        newname = file.replace(`${answers.match}`, `${answers.replacer}`);
+      } else {
+        if (!answers.match.includes('.') && file.split(`${answers.match}`)[0] === '') {
           newname = file.replace(`${answers.match}`, `${answers.replacer}`);
         }
-        else{
-          if(!answers.match.includes('.') && file.split(`${answers.match}`)[0] === ''){
-            newname = file.replace(`${answers.match}`, `${answers.replacer}`);
-          }
-          if(!answers.match.includes('.') && file.split(`${answers.match}`)[0] !== ''){
-            newname = file.replace(`${answers.match}`, `${answers.replacer}`);
-          }
-          else{
-            return
-          }
+        if (!answers.match.includes('.') && file.split(`${answers.match}`)[0] !== '') {
+          newname = file.replace(`${answers.match}`, `${answers.replacer}`);
         }
-        return rename(
-          `${dirname}/${file}`,
-            `${dirname}/${newname}`,
-            (err, _data) => {
-              if (err) return err;
-              console.log(`${color.green(`${file}`)} => ${color.magenta(`${newname}`)}`)
-            }
-        );
       }
+      return rename(
+        `${dirname}/${file}`,
+        `${dirname}/${newname}`,
+        (err, _data) => {
+          if (err) return err;
+          console.log(`${color.green(`${file}`)} => ${color.magenta(`${newname}`)}`)
+        }
+      );
     }
+  }
 }
