8065673d3adc5ccbfdab55127e29e8b29c6a5df9
knowbee <knowbeeinc@gmail.com>
2020-02-25 17:25:42 +0200

    rename middle characters

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 #!/usr/bin/env node
 
 const fs = require("fs")
+const { basename } = require('path')
+
 const {
   askCommands,
 } = require('./src/cli');
 const {opendirs} = require("./src/opendirs")
-const {helper} = require("./src/help")
-helper()
+
+// check to see if required arguments are given
+if (basename(process.argv[1]) === 'index.js' && process.argv.length !== 4) {
+  console.error(`Usage: node ${basename(process.argv[1])} --path <Path Name>`)
+  process.exit(1)
+}
+if(basename(process.argv[1]) !== 'index.js' && process.argv.length !== 3){
+    console.error(`Usage: rename --path <Path Name>`)
+    process.exit(1)
+}
 
 process.argv.slice(2).forEach(function(cmd) {
   if (cmd === "--path" || cmd === "-p") {
--- src/renamer.js
+++ src/renamer.js
@@ -2,27 +2,39 @@ const color  = require('chalk')
 const { rename } = require("fs");
 
 module.exports = {
+  /**
+ * Renames file names
+ *
+ * @param {String} file
+ * @param {String} answers
+ * @param {String} dirname
+ * @returns {<String>} A new file name.
+ */
    renamer : (file, answers, dirname) =>{
-    if (file.match(`${answers.match}`)) {
-      let newname
-      if(answers.match.includes('.') && file.split(`${answers.match}`)[1] === ''){
-        newname = file.replace(`${answers.match}`, `${answers.replacer}`);
-      }
-      else{
-        if(!answers.match.includes('.') && file.split(`${answers.match}`)[0] === ''){
+      if (file.match(`${answers.match}`)) {
+        let newname
+        if(answers.match.includes('.') && file.split(`${answers.match}`)[1] === ''){
           newname = file.replace(`${answers.match}`, `${answers.replacer}`);
-        }else{
-          return
         }
-      }
-      return rename(
-        `${dirname}/${file}`,
-          `${dirname}/${newname}`,
-          (err, _data) => {
-            if (err) return err;
-            console.log(`${color.green(`${file}`)} => ${color.magenta(`${newname}`)}`)
+        else{
+          if(!answers.match.includes('.') && file.split(`${answers.match}`)[0] === ''){
+            newname = file.replace(`${answers.match}`, `${answers.replacer}`);
           }
-      );
+          if(!answers.match.includes('.') && file.split(`${answers.match}`)[0] !== ''){
+            newname = file.replace(`${answers.match}`, `${answers.replacer}`);
+          }
+          else{
+            return
+          }
+        }
+        return rename(
+          `${dirname}/${file}`,
+            `${dirname}/${newname}`,
+            (err, _data) => {
+              if (err) return err;
+              console.log(`${color.green(`${file}`)} => ${color.magenta(`${newname}`)}`)
+            }
+        );
+      }
     }
-  }
 }
