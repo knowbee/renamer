cc8d191c56f0c7fef8b232b1fef69f348d897f58
Googler <knowbeeinc@gmail.com>
2020-03-03 09:17:22 +0200

    fix extension duplicates

--- src/renamer.js
+++ src/renamer.js
@@ -1,38 +1,39 @@
-const color = require('chalk')
-const {
-  rename
-} = require("fs");
+const color = require("chalk");
+const { rename } = require("fs");
 
 module.exports = {
-  /**
-   * Renames file names
-   *
-   * @param {String} file
-   * @param {String} answers
-   * @param {String} dirname
-   * @returns {<String>} A new file name.
-   */
   renamer: (file, answers, dirname) => {
     if (file.match(`${answers.match}`)) {
-      let newname
-      if (answers.match.includes('.') && file.split(`${answers.match}`)[1] === '') {
+      let newname;
+      if (
+        answers.match.includes(".") &&
+        file.split(`${answers.match}`)[1] === ""
+      ) {
+        newname = file.replace(`${answers.match}`, `${answers.replacer}`);
+      } else if (
+        !answers.match.includes(".") &&
+        file.split(`${answers.match}`)[0] === ""
+      ) {
+        newname = file.replace(`${answers.match}`, `${answers.replacer}`);
+      } else if (
+        !answers.match.includes(".") &&
+        file.split(`${answers.match}`)[0] !== ""
+      ) {
         newname = file.replace(`${answers.match}`, `${answers.replacer}`);
       } else {
-        if (!answers.match.includes('.') && file.split(`${answers.match}`)[0] === '') {
-          newname = file.replace(`${answers.match}`, `${answers.replacer}`);
-        }
-        if (!answers.match.includes('.') && file.split(`${answers.match}`)[0] !== '') {
-          newname = file.replace(`${answers.match}`, `${answers.replacer}`);
-        }
+        return;
       }
+
       return rename(
         `${dirname}/${file}`,
         `${dirname}/${newname}`,
-        (err, _data) => {
+        (err, data) => {
           if (err) return err;
-          console.log(`${color.green(`${file}`)} => ${color.magenta(`${newname}`)}`)
+          console.log(
+            `${color.green(`${file}`)} => ${color.magenta(`${newname}`)}`
+          );
         }
       );
     }
   }
-}
+};
