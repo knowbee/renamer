6f1d6f3896ce0b4343252a135b9302fd81e6eca4
Googler <knowbeeinc@gmail.com>
2020-03-10 20:20:10 +0200

    version 1.0.4

--- src/help.js
+++ src/help.js
@@ -9,7 +9,7 @@ module.exports = {
         `rename multiple files with by filenames,patterns or extensions
       `
       )
-      .version("1.0.3")
+      .version("1.0.4")
       .option("--path, -p", "provide a path to run renamer")
       .parse(process.argv);
     renamer.on("--help", () => {
