142efb36ef64a98586d9b30773397451fe44fc26
Googler <knowbeeinc@gmail.com>
2020-03-10 19:19:47 +0200

    update cli

--- src/help.js
+++ src/help.js
@@ -1,9 +1,9 @@
 const cli = require("commander");
 
-const rename = new cli.Command();
+const renamer = new cli.Command();
 module.exports = {
   helper: () => {
-    rename
+    renamer
       .name("@knowbee/rename")
       .description(
         `rename multiple files with by filenames,patterns or extensions
@@ -12,7 +12,7 @@ module.exports = {
       .version("1.0.1")
       .option("--path, -p", "provide a path to run renamer")
       .parse(process.argv);
-    rename.on("--help", () => {
+    renamer.on("--help", () => {
       console.log("How to use renamer:");
       console.log("  $ rename --help");
       console.log("  $ rename --path");
